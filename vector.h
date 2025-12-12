1ba1bc6340edaa1e642f5e70f78f25f152a42589
Анжелика <anhamenkova@gmail.com>
2025-12-13 02:45:24 +0300

    vector.h

--- vector.h
+++ vector.h
@@ -6,11 +6,22 @@ typedef struct {
     int size;
     int max;
 } vector;
-
-
-
-
-
-
-
+void vector_init(vector* v);
+int vector_size(vector* v);
+building* vector_get(vector* v, int index);
+building* vector_begin(vector* v);
+building* vector_end(vector* v) ;
+building* vector_next(vector* v, building* current);
+building* vector_to(vector* v, building* current);
+static void vector_resize(vector* v, int new_max);
+void vector_push_back(vector* v, building* b);
+void vector_insert(vector* v, int index, building* b);
+void vector_push_front(vector* v, building* b);
+void vector_remove(vector* v, int index);
+void vector_pop_front(vector* v);
+void vector_pop_back(vector* v) ;
+void vector_swap(vector* v, int i, int j);
+void vector_clear(vector* v) ;
+void vector_from_array(vector* v, building* array, int n) ;
+building* vector_to_array(vector* v);
 #endif
